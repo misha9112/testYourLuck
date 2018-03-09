1c53964761517921a13419de18255193bb9b0911
022 <022@192.168.0.62>
2018-03-09 16:46:53 +0900

    JTextArea 추가

--- Project/src/com/inc/project/v2/TestYourLuck.java
+++ Project/src/com/inc/project/v2/TestYourLuck.java
@@ -1,9 +1,10 @@
 package com.inc.project.v2;
 
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 public class TestYourLuck extends JFrame{
-	
+	JTextArea questionArea;
 	
 	public static void main(String[] args) {
 		

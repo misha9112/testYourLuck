50c017f66ac638b2059ee75a3c92f988ece9ed03
022 <022@192.168.0.62>
2018-03-09 17:05:43 +0900

    Frame 제작

--- Project/src/com/inc/project/v2/TestYourLuck.java
+++ Project/src/com/inc/project/v2/TestYourLuck.java
@@ -8,8 +8,16 @@ public class TestYourLuck extends JFrame{
 	JTextArea questionArea;
 	JButton yesBtn;
 	JButton noBtn;
-	int a;
+	public TestYourLuck() {
+		setTitle("Main Frame");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setBounds(300, 300, 500, 500);
+		setResizable(false);
+
+		setVisible(true);
+	}
+
 	public static void main(String[] args) {
-		
+		new TestYourLuck();
 	}
 }

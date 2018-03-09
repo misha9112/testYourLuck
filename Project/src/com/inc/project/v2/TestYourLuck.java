d5cf93e495b715343cfbc482de5a8d3266e685cc
022 <022@192.168.0.62>
2018-03-09 16:49:24 +0900

    btn, area추가

--- Project/src/com/inc/project/v2/TestYourLuck.java
+++ Project/src/com/inc/project/v2/TestYourLuck.java
@@ -1,11 +1,13 @@
 package com.inc.project.v2;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
 public class TestYourLuck extends JFrame{
 	JTextArea questionArea;
-	
+	JButton yesBtn;
+	JButton noBtn;
 	public static void main(String[] args) {
 		
 	}

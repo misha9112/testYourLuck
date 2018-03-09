3e3f17fd53162bcd34867193a83bf1494fdca60a
022 <022@192.168.0.62>
2018-03-09 16:41:20 +0900

    hi

---
+++ Project/src/com/inc/project/v1/TestYourLuck.java
@@ -0,0 +1,32 @@
+package com.inc.project.v1;
+
+import java.util.Scanner;
+
+public class TestYourLuck {
+	public static void main(String[] arg0) {
+		Scanner scanner = new Scanner(System.in);
+		int a;
+		int ran = (int) (Math.random() * 2 + 1);
+		int i = 10;
+		int dcnt = 0;
+		System.out.println("Game Start!");
+		while (i > 1) {
+			System.out.println("1. Yes\n2. No\n3. Exit");
+			a = scanner.nextInt();
+			System.out.println(ran);
+
+			if (ran == a) {
+				System.out.println("You Die");
+				i = 10;
+				ran = (int) (Math.random() * 2 + 1);
+				dcnt++;
+			} else if (a == 3 || dcnt == 100) {
+				System.out.println("Death Count : " + dcnt);
+				return;
+			}
+			ran = (int) (Math.random() * 2 + 1);
+			i--;
+		}
+
+	}
+}
---
+++ Project/src/com/inc/project/v2/TestYourLuck.java
@@ -0,0 +1,11 @@
+package com.inc.project.v2;
+
+import javax.swing.JFrame;
+
+public class TestYourLuck extends JFrame{
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}

315c8400ae8a30307b5b7ac337618e1e42c87d5b
022 <022@192.168.0.62>
2018-03-09 17:10:31 +0900

    method 추가

--- Project/src/com/inc/project/v2/TestYourLuck.java
+++ Project/src/com/inc/project/v2/TestYourLuck.java
@@ -8,13 +8,30 @@ public class TestYourLuck extends JFrame{
 	JTextArea questionArea;
 	JButton yesBtn;
 	JButton noBtn;
+	
+	
 	public TestYourLuck() {
 		setTitle("Main Frame");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setBounds(300, 300, 500, 500);
 		setResizable(false);
-
+		
+		init();
+		
 		setVisible(true);
+	}
+
+	public void init() {
+		initComponent();  
+		initEvent();
+	}
+
+	private void initEvent() {
+		
+	}
+
+	private void initComponent() {
+		
 	}
 
 	public static void main(String[] args) {
